5016866a11877ea0e14d6db30343fbb7eb591911
V3NOM1977 <rishabhrawat1977@gmail.com>
2023-09-03 20:26:27 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.example.models.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Project Started...!!");
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        FileInputStream fileInputStream = new FileInputStream(new File("src/main/resources/image.png"));
+        byte[] image = new byte[fileInputStream.available()];
+        fileInputStream.read(image);
+
+        Student student = Student.builder()
+                .studentName("Rishabh Rawat")
+                .rollNo(67)
+                .createdAt(new Date())
+                .image(image)
+                .temp(100)
+                .build();
+
+        Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(student);
+
+        transaction.commit();
+        session.close();
+    }
+
+}
